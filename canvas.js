d9a4f5462176f08fd88b2090e782d864e3aaf953
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-09 15:06:19 -0400

    Draw a random blue ball

--- canvas.js
+++ canvas.js
@@ -5,9 +5,9 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 //Rectangles
-c.fillStyle = '#ffde05';
+c.fillStyle = 'gold';
 c.fillRect(300, 300, 100, 100);
-c.fillStyle = 'red';
+c.fillStyle = 'maroon';
 c.fillRect(335, 340, 30, 60);
 
 //Lines
@@ -15,5 +15,14 @@ c.beginPath();
 c.moveTo(300, 300);
 c.lineTo(350, 250);
 c.lineTo(400, 300);
-c.strokeStyle = 'red';
+c.lineTo(300, 300);
+c.strokeStyle = 'maroon';
 c.stroke();
+
+//Circles
+var x = Math.random() * window.innerWidth;
+var y = Math.random() * window.innerHeight;
+c.beginPath();
+c.fillStyle = 'steelblue';
+c.arc(x, y, 5, Math.PI * 2, false);
+c.fill();
