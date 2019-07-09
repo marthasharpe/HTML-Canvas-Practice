eb25e9c1082379f16094d2348c246c53eace022f
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-09 15:54:34 -0400

    Make the blue ball bounce

--- canvas.js
+++ canvas.js
@@ -4,25 +4,43 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-//Rectangles
-c.fillStyle = 'gold';
-c.fillRect(300, 300, 100, 100);
-c.fillStyle = 'maroon';
-c.fillRect(335, 340, 30, 60);
-
-//Lines
-c.beginPath();
-c.moveTo(300, 300);
-c.lineTo(350, 250);
-c.lineTo(400, 300);
-c.lineTo(300, 300);
-c.strokeStyle = 'maroon';
-c.stroke();
-
-//Circles
 var x = Math.random() * window.innerWidth;
 var y = Math.random() * window.innerHeight;
-c.beginPath();
-c.fillStyle = 'steelblue';
-c.arc(x, y, 5, Math.PI * 2, false);
-c.fill();
+var dx = 0;
+var dy = 4;
+var radius = 5;
+
+//Animation Loop
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight); //clears entire canvas
+    
+    //Rectangles
+    c.fillStyle = 'gold';
+    c.fillRect(300, 300, 100, 100);
+    c.fillStyle = 'maroon';
+    c.fillRect(335, 340, 30, 60);
+
+    //Lines
+    c.beginPath();
+    c.moveTo(300, 300);
+    c.lineTo(350, 250);
+    c.lineTo(400, 300);
+    c.lineTo(300, 300);
+    c.strokeStyle = 'maroon';
+    c.stroke();
+
+    //Circles
+    c.beginPath();
+    c.fillStyle = 'steelblue';
+    c.arc(x, y, radius, Math.PI * 2, false);
+    c.fill();
+
+    if (y + radius > innerHeight || y - radius < 0) {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
+}
+
+animate();
