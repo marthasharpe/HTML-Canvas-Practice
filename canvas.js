89731f9868901870c22d2b41fddd28bde54a9e11
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-09 14:43:25 -0400

    Draw a little yellow house

--- canvas.js
+++ canvas.js
@@ -4,4 +4,16 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillRect(100, 100, 100, 100);
+//Rectangles
+c.fillStyle = '#ffde05';
+c.fillRect(300, 300, 100, 100);
+c.fillStyle = 'red';
+c.fillRect(335, 340, 30, 60);
+
+//Lines
+c.beginPath();
+c.moveTo(300, 300);
+c.lineTo(350, 250);
+c.lineTo(400, 300);
+c.strokeStyle = 'red';
+c.stroke();
