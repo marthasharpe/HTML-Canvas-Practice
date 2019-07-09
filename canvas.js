5dc3ac8b3eec0f8dd02854e7d2bf0ba34d24387a
Martha Sharpe <marthasharpe2020@gmail.com>
2019-07-08 22:03:00 -0400

    Import context variable

--- canvas.js
+++ canvas.js
@@ -1,2 +1,7 @@
 var canvas = document.querySelector('canvas');
-console.log(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100);
